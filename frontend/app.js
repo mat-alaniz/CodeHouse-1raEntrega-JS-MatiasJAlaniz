ac3b24e2a3fa589152ee127682731c3568185f0f
Matias Alaniz <matias.cai.alaniz@gmail.com>
2024-11-07 20:58:53 -0300

    primer entrega

---
+++ frontend/app.js
@@ -0,0 +1,27 @@
+let entrada = prompt("where are you from?");
+
+while(entrada != "ESC" ){
+   switch (entrada) {
+       case "Argentina":
+            alert("Hola Argentino");
+            break;
+        case "France":
+            alert("bonjour français");
+            break;
+            //idioma chino tradicional
+        case "中國":
+            alert("歡迎中國人");
+            break;
+            //idioma tailandes
+        case "ประเทศไทย":
+            alert("สวัสดี แบบไทย");
+            break;
+        case "Mexico":
+            alert("Viva mexico cabrones!!");
+            break;
+       default:
+           alert("where did you visit us from?")
+           break;
+   }
+   entrada = prompt("where are you from?");
+}
